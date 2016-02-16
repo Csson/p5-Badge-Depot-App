c64807728e8b1e4e99f87ae0a5e440cbf1f2e41a
Erik Carlsson <info@code301.com>
2016-02-16 18:32:49 +0000

    Dev

--- lib/Badge/Depot/App/Command/initdb.pm
+++ lib/Badge/Depot/App/Command/initdb.pm
@@ -5,7 +5,7 @@ use warnings;
 package Badge::Depot::App::Command::initdb;
 
 # AUTHORITY
-our $VERSION = '0.0002';
+our $VERSION = '0.0001';
 
 use Mojo::Base 'Mojolicious::Command';
 use Getopt::Long qw/GetOptionsFromArray/;
